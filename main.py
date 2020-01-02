66a98250f459a2742f81b0051abd8dfd62adf55e
JasonZhang <zhangfan@k2data.com.cn>
2020-01-02 13:00:57 +0800

    容错处理

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from multiprocessing.dummy import Pool as Threadpool
 from urllib import parse
 
 import requests
+import os
 
 #  001 ~ 821
 tmp = parse.quote('玄幻奇幻/庆余年_秋水雁翎')
@@ -10,12 +11,16 @@ target_url = [
     for i in range(821)
 ]
 
-PATH = '/Users/xxx/Downloads/庆余年语音小说/'
+PATH = '/Users/jasonzhang/Downloads/庆余年语音小说/'
 
 
 def download(url):
+    path = PATH + url.split('/')[-1]
+    if os.path.exists(path) and os.path.getsize(path) > 10:
+        return
+
     res = requests.get(url, stream=False)
-    with open(PATH + url.split('/')[-1], "wb") as f:
+    with open(path, "wb") as f:
         f.write(res.content)
     print(url.split('/')[-1])
 
